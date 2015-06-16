b7dc51be3ec046525b59ff12590939c441538679
Vincent Verhoeven <verhoevenv@gmail.com>
2015-06-16 16:41:01 +0200

    highlighting for conditional groupings

--- src/com/github/verhoevenv/intellij/jess/JessAnnotator.java
+++ src/com/github/verhoevenv/intellij/jess/JessAnnotator.java
@@ -1,5 +1,6 @@
 package com.github.verhoevenv.intellij.jess;
 
+import com.github.verhoevenv.intellij.jess.psi.JessConditionhead;
 import com.github.verhoevenv.intellij.jess.psi.JessSlotname;
 import com.intellij.lang.annotation.Annotation;
 import com.intellij.lang.annotation.AnnotationHolder;
@@ -8,12 +9,26 @@ import com.intellij.openapi.editor.DefaultLanguageHighlighterColors;
 import com.intellij.psi.PsiElement;
 import org.jetbrains.annotations.NotNull;
 
+import java.util.List;
+
+import static java.util.Arrays.asList;
+
 public class JessAnnotator implements Annotator {
+
+    private final List<String> conditionalGroupings = asList("not", "or", "and");
+
     @Override
     public void annotate(@NotNull PsiElement element, @NotNull AnnotationHolder holder) {
         if(element instanceof JessSlotname) {
             Annotation infoAnnotation = holder.createInfoAnnotation(element, null);
             infoAnnotation.setTextAttributes(DefaultLanguageHighlighterColors.INSTANCE_FIELD);
+        }
+        if(element instanceof JessConditionhead) {
+            JessConditionhead conditionhead = (JessConditionhead) element;
+            if(conditionalGroupings.contains(conditionhead.getSymbol().getText())) {
+                Annotation infoAnnotation = holder.createInfoAnnotation(element, null);
+                infoAnnotation.setTextAttributes(DefaultLanguageHighlighterColors.PREDEFINED_SYMBOL);
+            }
         }
     }
 }
