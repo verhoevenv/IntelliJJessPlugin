8c5aa31cba9b64f3a738f7c19821c53b5a43ba4d
Vincent Verhoeven <verhoevenv@gmail.com>
2015-06-16 17:23:16 +0200

    highlighting for (some) slot names in conditionals
    New style matching not supported yet - that would need way more structure

--- src/com/github/verhoevenv/intellij/jess/JessAnnotator.java
+++ src/com/github/verhoevenv/intellij/jess/JessAnnotator.java
@@ -1,12 +1,15 @@
 package com.github.verhoevenv.intellij.jess;
 
 import com.github.verhoevenv.intellij.jess.psi.JessConditionhead;
+import com.github.verhoevenv.intellij.jess.psi.JessRuleconditionlist;
 import com.github.verhoevenv.intellij.jess.psi.JessSlotname;
+import com.github.verhoevenv.intellij.jess.psi.JessTypes;
 import com.intellij.lang.annotation.Annotation;
 import com.intellij.lang.annotation.AnnotationHolder;
 import com.intellij.lang.annotation.Annotator;
 import com.intellij.openapi.editor.DefaultLanguageHighlighterColors;
 import com.intellij.psi.PsiElement;
+import com.intellij.psi.PsiWhiteSpace;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.List;
@@ -20,15 +23,53 @@ public class JessAnnotator implements Annotator {
     @Override
     public void annotate(@NotNull PsiElement element, @NotNull AnnotationHolder holder) {
         if(element instanceof JessSlotname) {
-            Annotation infoAnnotation = holder.createInfoAnnotation(element, null);
-            infoAnnotation.setTextAttributes(DefaultLanguageHighlighterColors.INSTANCE_FIELD);
+            highlightAsSlotName(element, holder);
         }
         if(element instanceof JessConditionhead) {
             JessConditionhead conditionhead = (JessConditionhead) element;
             if(conditionalGroupings.contains(conditionhead.getSymbol().getText())) {
                 Annotation infoAnnotation = holder.createInfoAnnotation(element, null);
                 infoAnnotation.setTextAttributes(DefaultLanguageHighlighterColors.PREDEFINED_SYMBOL);
+            } else {
+                highlightFollowingSlots(conditionhead, holder);
             }
         }
+    }
+
+    private void highlightAsSlotName(@NotNull PsiElement element, @NotNull AnnotationHolder holder) {
+        Annotation infoAnnotation = holder.createInfoAnnotation(element, null);
+        infoAnnotation.setTextAttributes(DefaultLanguageHighlighterColors.INSTANCE_FIELD);
+    }
+
+    private void highlightFollowingSlots(JessConditionhead conditionhead, AnnotationHolder holder) {
+        PsiElement[] siblings = conditionhead.getParent().getChildren();
+        for (PsiElement sibling : siblings) {
+            if(sibling instanceof JessRuleconditionlist) {
+                JessRuleconditionlist conditionlist = (JessRuleconditionlist) sibling;
+                if(!isInFunctionContext(conditionlist)) {
+                    highlightAsSlotName(conditionlist.getConditionhead(), holder);
+                }
+
+            }
+        }
+    }
+
+    private boolean isInFunctionContext(JessRuleconditionlist element) {
+        return getPreviousNonWhitspaceSibling(element).getText().equals(":") || wrappingListIsInFunctionContext(element);
+    }
+
+    private boolean wrappingListIsInFunctionContext(JessRuleconditionlist element) {
+        if(element.getParent() instanceof JessRuleconditionlist) {
+            return isInFunctionContext((JessRuleconditionlist) element.getParent());
+        }
+        return false;
+    }
+
+    private PsiElement getPreviousNonWhitspaceSibling(JessRuleconditionlist element) {
+        PsiElement prevSibling = element.getPrevSibling();
+        while(prevSibling != null && prevSibling instanceof PsiWhiteSpace) {
+            prevSibling = prevSibling.getPrevSibling();
+        }
+        return prevSibling;
     }
 }
